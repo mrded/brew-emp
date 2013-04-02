77a3f67500dce7ae1260bb49bc740a22c771edac
Dmitry Demenchuk <dmitry@demenchuk.me>
2013-04-02 15:13:22 +0100

    Add configs

--- install.sh
+++ install.sh
@@ -7,6 +7,17 @@ mysql_install_db --verbose --user=`whoami` --basedir="$(brew --prefix mysql)" --
 echo '✩✩✩✩ NGINX ✩✩✩✩'
 brew install nginx
 
+# @TODO: Download nginx.conf to /usr/local/etc/nginx/nginx.conf
+
+mkdir /usr/local/etc/nginx/{common,sites-available,sites-enabled}
+# @TODO: Download drupal to /usr/local/etc/nginx/common/drupal
+
+# @TODO: Download default to /usr/local/etc/nginx/sites-available/default 
+ln -s /usr/local/etc/nginx/sites-available/default /usr/local/etc/nginx/sites-enabled/default
+
+# Create folder for logs.
+mkdir -p /usr/local/var/log/{fpm,nginx}
+
 echo '✩✩✩✩ PHP + FPM ✩✩✩✩'
 brew tap homebrew/dupes
 brew tap josegonzalez/homebrew-php
