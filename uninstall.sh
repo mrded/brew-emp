af73a3e9b5277c32104e766bd7c15d67b0d0635e
Dmitry Demenchuk <dmitry@demenchuk.me>
2015-09-30 23:45:53 +0100

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -13,6 +13,26 @@ brew remove php54
 brew remove php55
 brew remove php56
 
+brew remove php53-memcached
+brew remove php54-memcached
+brew remove php55-memcached
+brew remove php56-memcached
+
+brew remove php53-redis
+brew remove php54-redis
+brew remove php55-redis
+brew remove php56-redis
+
+brew remove php53-xdebug
+brew remove php54-xdebug
+brew remove php55-xdebug
+brew remove php56-xdebug
+
+brew remove php53-xhprof
+brew remove php54-xhprof
+brew remove php55-xhprof
+brew remove php56-xhprof
+
 rm /usr/local/bin/brew-emp
 
 echo "Done!"
