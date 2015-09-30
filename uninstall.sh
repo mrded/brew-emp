42fa458dd130bdc50dd188bf4b7dbee6ff78bf65
Dmitry Demenchuk <dmitry@demenchuk.me>
2015-09-30 23:40:12 +0100

    Create uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+brew remove mariadb
+brew remove nginx
+
+brew remove freetype
+brew remove jpeg
+brew remove libpng
+brew remove gd
+brew remove drush
+
+brew remove php53
+brew remove php54
+brew remove php55
+brew remove php56
