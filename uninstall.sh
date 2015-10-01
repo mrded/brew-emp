7cbc489cd3a62300e322c4ca9ed639e39dafbdc3
Dmitry Demenchuk <dmitry@demenchuk.me>
2015-10-01 12:52:38 +0100

    Refactor.

--- uninstall.sh
+++ uninstall.sh
@@ -6,32 +6,18 @@ brew remove freetype
 brew remove jpeg
 brew remove libpng
 brew remove gd
+brew remove graphviz
 brew remove drush
 
-brew remove php53
-brew remove php54
-brew remove php55
-brew remove php56
-
-brew remove php53-memcached
-brew remove php54-memcached
-brew remove php55-memcached
-brew remove php56-memcached
-
-brew remove php53-redis
-brew remove php54-redis
-brew remove php55-redis
-brew remove php56-redis
-
-brew remove php53-xdebug
-brew remove php54-xdebug
-brew remove php55-xdebug
-brew remove php56-xdebug
-
-brew remove php53-xhprof
-brew remove php54-xhprof
-brew remove php55-xhprof
-brew remove php56-xhprof
+PHP_VERSIONS=( 53 54 55 56 )
+for PHP_VERSION in "${PHP_VERSIONS[@]}"
+do
+  brew remove php"$PHP_VERSION"
+  brew remove php"$PHP_VERSION"-memcached
+  brew remove php"$PHP_VERSION"-redis
+  brew remove php"$PHP_VERSION"-xdebug
+  brew remove php"$PHP_VERSION"-xhprof
+done
 
 rm /usr/local/bin/brew-emp
 
