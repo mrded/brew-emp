516fa3ed0c0304614cea522bbf9b6d86783aded0
Dmitry Demenchuk <dmitry@demenchuk.me>
2015-10-07 10:57:44 +0100

    Update install.sh

--- tools/install.sh
+++ tools/install.sh
@@ -51,6 +51,7 @@ echo '✩✩✩✩ MYSQL (mariadb) ✩✩✩✩'
 brew install mariadb
 #mysql_install_db --verbose --user=`whoami` --basedir="$(brew --prefix mariadb)" --datadir=/usr/local/var/mysql --tmpdir=/tmp
 curl -Lo /usr/local/etc/my.cnf https://raw.github.com/mrded/brew-emp/master/conf/mysql/my.cnf
+chmod 644 /usr/local/etc/my.cnf
 
 echo '✩✩✩✩ NGINX ✩✩✩✩'
 brew install --with-passenger nginx
